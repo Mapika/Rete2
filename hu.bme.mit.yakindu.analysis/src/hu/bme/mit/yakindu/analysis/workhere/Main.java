517c91a06f21232754bb8e3ed58002129d7e50f5
Mapika <marosimark251@gmail.com>
2021-03-13 13:29:48 +0100

    Kettes feladat.

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,10 +1,14 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.util.UUID;
+
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Transition;
 import org.yakindu.sct.model.sgraph.Statechart;
+
 
 import hu.bme.mit.model2gml.Model2GML;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
@@ -29,7 +33,18 @@ public class Main {
 			EObject content = iterator.next();
 			if(content instanceof State) {
 				State state = (State) content;
-				System.out.println(state.getName());
+				if(state.getOutgoingTransitions().size()==0) {
+					//System.out.println(state.getName());
+				}
+				if (state.getName().isEmpty()) {
+					System.out.println(state.getName()+" suggested name: "+ UUID.randomUUID().toString().replace("-",""));
+				}
+				//System.out.println(state.getName()); 
+			}
+			else if (iterator.hasNext()) {
+				if (content instanceof Transition) {
+					//System.out.println(((Transition) content).getSource().getName() + " -> " + ((Transition) content).getTarget().getName());
+				}
 			}
 		}
 		
